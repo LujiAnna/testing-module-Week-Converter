90dddc6fb973131fee1f4fc2665f4b7e6968e83e
abdulhamid <abdulhamid.ali.996@gmail.com>
2020-07-23 17:47:01 +0200

    converting-to-hours closed#11

---
+++ scripts/user-actions/convert-hours.js
@@ -0,0 +1,15 @@
+
+function hours_converterHandler (){
+  weeks=6;
+    let hours=weeks*7*24;
+    if(weeks!==0)
+    alert(`the weeks in hours `+hours+'h');
+    else if(weeks===0)
+    alert(`please put the numbers of  weeks `);
+    else if(typeof weeks !=='number')
+    alert(`please put a number`);
+
+    console.log('numbers of weeks '+weeks);
+    console.log('weeks in hours '+hours+'h');
+}
+ 
