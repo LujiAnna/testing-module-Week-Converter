cf58ff1b16a77815ed82e3439fbb7bf0f3fb803d
abdulhamid <abdulhamid.ali.996@gmail.com>
2020-07-25 00:41:38 +0200

    minutes-handler closed #11

--- scripts/user-actions/convert-hours.js
+++ scripts/user-actions/convert-hours.js
@@ -1,11 +1,11 @@
 function hoursHandler (){
 
   let hours=weeks*7*24;
-  let message = `${weeks} week is equal to ${seconds} seconds`;
+  let message = `${weeks} week is equal to ${hours} hours`;
   alert(message);
   console.log(`weeks: ${weeks}, \ntypeof weeks: ${typeof(weeks)}`);
-  console.log(`seconds: 604800*${weeks}`);
-  console.log(`seconds: ${seconds}, \ntypeof seconds: ${typeof(seconds)}`);
+  console.log(`hours: 168*${weeks}`);
+  console.log(`hours: ${hours}, \ntypeof hours: ${typeof(hours)}`);
   console.log(`message: ${message}`);
 };
-console.log('--- finished: convert-seconds.js');
+console.log('--- finished: convert-hours.js');
